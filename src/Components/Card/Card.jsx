11c7e8e730206200d99c4babaf64247e87db5f2a
MohamedTlili <Mohamedtlili1995@hotmail.com>
2023-12-01 00:58:58 +0100

    almost-done

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react' 
+import "./Style.css";
+
+function Card() {
+  return (
+    <div>
+        <div>
+            <img src="" alt="" />
+        </div>
+       <h6></h6>
+        <p></p>
+    </div>
+  )
+}
+
+export default Card
--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,12 +1,16 @@
-import React from "react";
 import "./Style.css";
+import React from "react";
 import Button from "../Button/Button";
+import Card from "../Card/Card"
 function Intro() {
   return (
     <div className="intro">
       <div className="intro-containr">
       <h2>Latest Movies</h2>
       <Button content="View All"/>
+      </div>
+      <div>
+        <Card/>
       </div>
     </div>
   );
