189c3d251c4b6a3fd53edfdce342cd9c6fb1e0ec
MohamedTlili <Mohamedtlili1995@hotmail.com>
2023-12-01 12:54:05 +0100

    almost-done

--- src/Components/FooterSection/FooterSection.jsx
+++ src/Components/FooterSection/FooterSection.jsx
@@ -1,16 +1,44 @@
 import "./Style.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
 
-import React from 'react'
+import React from "react";
 
 function FooterSection() {
   return (
     <div className="footerSection">
-        <div className="footer-containr">
-        <span className="youhover" style={{color:"white"}}>You</span>
-        <span style={{color:"red"}}>video.</span>
+      <div className="footer-containr">
+        <span className="you" style={{ color: "white" }}>
+          You
+        </span>
+        <span style={{ color: "red" }}>video.</span>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's
+        </p>
+        <div className="icons">
+        <FaFacebookF  color="#777" />
+        <FaTwitter color="#777" />
+        <FaLinkedinIn color="#777" />
+        <FaTelegram color="#777" />
         </div>
+        
+      </div>
+      <div>
+        <h4>Movies</h4>
+        <p>Drama</p>
+        <p>Action</p>
+        <p>Animation</p>
+        <p>Comedy</p>
+        <p>Crime</p>
+        <p>Fantacy</p>
+        <p>Horror</p>
+        <p>Romance</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default FooterSection
+export default FooterSection;
