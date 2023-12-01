5b224ed9185ad01dda56ec883042920983248da6
MohamedTlili <Mohamedtlili1995@hotmail.com>
2023-12-01 16:09:06 +0100

    almost-done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Hero from "./Components/Hero/Hero";
 import Intro from "./Components/Intro/Intro";
 import FooterSection from "./Components/FooterSection/FooterSection";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div>
       <Hero />
       <Intro />
       <FooterSection/>
+      <Footer/>
     </div>
   );
 }
