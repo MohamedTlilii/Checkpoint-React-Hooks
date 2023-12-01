5f36b7abc9ffd8311f47aa84e157cf368cef7a9a
MohamedTlili <Mohamedtlili1995@hotmail.com>
2023-12-01 15:21:45 +0100

    almost-done

--- src/Components/FooterSection/FooterSection.jsx
+++ src/Components/FooterSection/FooterSection.jsx
@@ -10,7 +10,7 @@ function FooterSection() {
   return (
     <div className="footerSection">
       <div className="footer-containr">
-        <span className="you" style={{ color: "white" }}>
+        <span className="you" style={{ color: "black" }}>
           You
         </span>
         <span style={{ color: "red" }}>video.</span>
@@ -19,24 +19,26 @@ function FooterSection() {
           industry. Lorem Ipsum has been the industry's
         </p>
         <div className="icons">
-        <FaFacebookF  color="#777" />
-        <FaTwitter color="#777" />
-        <FaLinkedinIn color="#777" />
-        <FaTelegram color="#777" />
+          <FaFacebookF className="icon" color="#8f8f8f" fontSize="18px" />
+          <FaTwitter className="icon" color="#8f8f8f" fontSize="18px" />
+          <FaLinkedinIn className="icon" color="#8f8f8f" fontSize="18px" />
+          <FaTelegram className="icon" color="#8f8f8f"  fontSize="18px"/>
         </div>
-        
       </div>
-      <div>
+      <div className="footermovie">
         <h4>Movies</h4>
-        <p>Drama</p>
-        <p>Action</p>
-        <p>Animation</p>
-        <p>Comedy</p>
-        <p>Crime</p>
-        <p>Fantacy</p>
-        <p>Horror</p>
-        <p>Romance</p>
+       <div className="genrlist">
+       <p className="genre">Drama</p>
+        <p className="genre">Action</p>
+        <p className="genre">Animation</p>
+        <p className="genre">Comedy</p> <br />
+        <p className="genre">Crime</p>
+        <p className="genre">Fantacy</p>
+        <p className="genre">Horror</p>
+        <p className="genre">Romance</p>
+       </div>
       </div>
+      
     </div>
   );
 }
